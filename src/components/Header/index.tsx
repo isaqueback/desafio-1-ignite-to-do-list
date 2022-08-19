6bd2ea31b77d15d21b65d5132b76fe3912ed3de9
Isaque Delfino <isa.quecosta00@gmail.com>
2022-08-19 21:00:06 +0100

    fineshed header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,14 @@
+import styles from './styles.module.scss'
+import logo from '../../assets/logo.png'
+
 export function Header() {
     return (
-        <h1>Eu sou o cabeçalho!</h1>
+        <header className={styles.header}>
+            <img src={logo} alt="Logo" />
+            <div className={styles.appName}>
+                <span>to</span>
+                <span>do</span>
+            </div>
+        </header>
     )
 }
